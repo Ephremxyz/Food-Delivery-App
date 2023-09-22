558885ef02e06586108887601d142a94dc4f3916
Ephrem Getachew <gephrem130@gmail.com>
2023-09-22 19:18:56 +0300

    Update android.dart]

--- android.dart
+++ android.dart
@@ -5,7 +5,7 @@ void main() {
 }                                                      
                                                           
 class MyApp extends StatelessWidget {                                        
-  @override                    
+  @override                     
   Widget build(BuildContext context) {              
     return MaterialApp(      
       title: 'Derash Food Delivery App', 
