5685a856d818e02418c3b1361d57ce2591e88948
Ephrem Getachew <gephrem130@gmail.com>
2023-12-28 12:05:08 +0300

    Create List.js

---
+++ List.js
@@ -0,0 +1,42 @@
+// Registration
+const registerForm = document.getElementById("register-form");
+registerForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  // Validate user input
+  // Send registration data to the database
+});
+
+// Login
+const loginForm = document.getElementById("login-form");
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  // Validate user credentials
+  // Authenticate user with the database
+});
+// Fetch restaurants from the database
+fetch("/restaurants")
+  .then((response) => response.json())
+  .then((restaurants) => {
+    // Display restaurants on the page
+  });
+// Fetch menu items for a selected restaurant
+fetch("/restaurants/1/menu")
+  .then((response) => response.json())
+  .then((menuItems) => {
+    // Display menu items with prices and options
+  });
+// Add items to cart
+const addToCartButton = document.getElementById("add-to-cart");
+addToCartButton.addEventListener("click", () => {
+  // Get item details
+  // Store item in local storage or send to database
+});
+
+// Display cart items
+const cartItems = JSON.parse(localStorage.getItem("cart"));
+// Render cart items with quantity and total price
+// Collect user details (address, payment info)
+// Submit order to the database
+// Initiate payment processing (if integrated)
+// Fetch order status from the database
+// Display order status and estimated delivery time
