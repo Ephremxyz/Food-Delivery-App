27517284364a20e4d43b014489a32c3f0c259376
Ephrem Getachew <gephrem130@gmail.com>
2024-03-12 12:51:48 +0300

    Update Index.js

--- Index.js
+++ Index.js
@@ -3,7 +3,8 @@
 const registerForm = document.getElementById("register-form");
 registerForm.addEventListener("submit", (event) => {     m
   event.preventDefault();             
-  // Validate user input                               
+  // Validate user input                                
+                                                
   // Send registration data to the database                                                
 });                                                                                                
 // Login                                                                      
