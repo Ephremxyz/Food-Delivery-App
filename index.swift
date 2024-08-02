bec60a1fb85e989c35278ec6c90f51eacc4ae429
Ephrem Getachew <gephrem130@gmail.com>
2024-08-02 11:45:58 +0300

    Update index.swift

--- index.swift
+++ index.swift
@@ -8,7 +8,7 @@ struct DerashFoodDeliveryApp: View {
     VStack {                
       Text("Select a restaurant")    
       Picker(selection: $selectedRestaurant, label: Text("Restaurant")) {       
-        Text("Restaurant 1")   
+        Text("Restaurant 1")    
         Text("Restaurant 2")
         Text("Restaurant 3")
       }
