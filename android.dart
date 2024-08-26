aa74d47729d139e9d02c1fa690fd46018b176569
Ephrem Getachew <gephrem130@gmail.com>
2024-08-26 21:02:18 +0300

    Update android.dart

--- android.dart
+++ android.dart
@@ -23,7 +23,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold( 
       appBar: AppBar(
         title: Text('Food Delivery App'),
       ),
